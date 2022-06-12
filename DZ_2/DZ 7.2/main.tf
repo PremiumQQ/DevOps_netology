9a72cabe00f5a97f0c5044e1dd95fa83328e6b24
Mitasov_Igor <Igorin2012@gmail.com>
2022-06-12 09:41:23 +0600

    DZ 7.2

---
+++ DZ_2/DZ 7.2/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  service_account_key_file  = "/opt/terraform/key.json"
+  cloud_id  = "b1gn0391i1mdjc54t93u"
+  folder_id = "b1gdhtt160mqltnn39nu"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87va5cc00gaq2f5qfb"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
